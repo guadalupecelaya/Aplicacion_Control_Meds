ce5bc1a8b57545f298c132b74723aa71f243d8d1
GuadalupeCelaya <guadalupecelayap@gmail.com>
2022-09-16 17:19:27 -0500

    implementa barra lateral

--- app/src/main/java/com/example/app_medc/MainActivity.java
+++ app/src/main/java/com/example/app_medc/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.app_medc;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +30,66 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
 
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if(savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Medicamentos()).commit();
+            navigationView.setCheckedItem(R.id.nav_medicamento);
+        }
+    }
+
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_medicamento:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Medicamentos()).commit();
+                break;
+            case R.id.nav_enfermedades:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Enfermedades()).commit();
+                break;
+            case R.id.nav_estado:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Estado()).commit();
+                break;
+            case R.id.nav_perfil:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Perfil()).commit();
+                break;
+            case R.id.nav_Alarma:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Alarmas()).commit();
+                break;
+            case R.id.nav_liquido:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Liquido()).commit();
+                break;
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        }else{
+            super.onBackPressed();
+        }
     }
 
     public void logout(View view){
